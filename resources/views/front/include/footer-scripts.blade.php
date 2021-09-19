444393c740276bf5adc19bcfa01cfc46683b8577
mason.hows11 <mason.hows11@gmail.com>
2021-09-19 20:01:59 +0430

    front auth style updated

--- resources/views/front/include/footer-scripts.blade.php
+++ resources/views/front/include/footer-scripts.blade.php
@@ -1,1 +1,2 @@
-<?php
+<script src="{{ asset('js/js/jquery-3.5.1.min.js') }}"></script>
+<script src="{{ asset('js/js/bootstrap.bundle.min.js') }}"></script>
--- resources/views/front/include/header_style.blade.php
+++ resources/views/front/include/header_style.blade.php
@@ -1,1 +1,2 @@
-<?php
+<link rel="stylesheet" href="{{ asset('css/css/bootstrap.rtl.min.css') }}">
+<link rel="stylesheet" href="{{ asset('css/css/style.css') }}">
