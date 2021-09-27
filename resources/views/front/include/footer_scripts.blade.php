0711c86ed7cc11a6981268d9bdd574790add87cc
mason.hows11 <mason.hows11@gmail.com>
2021-09-27 14:58:57 +0330

    send email from gmail acc fixed 90%

--- resources/views/front/include/footer_scripts.blade.php
+++ resources/views/front/include/footer_scripts.blade.php
@@ -1,5 +1,13 @@
 <script src="{{ asset('js/app.js') }}"></script>
 <script src="{{ asset('js/scripts.js') }}"></script>
+<script>
+    $(document).ready(function () {
+        $('.alert-success').fadeIn().delay(3000).fadeOut();
+    });
+    $(document).ready(function () {
+        $('.alert-warning').fadeIn().delay(3000).fadeOut();
+    });
+</script>
 @yield('custome-script')
 </body>
 </html>
