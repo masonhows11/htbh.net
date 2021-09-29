2e0a94d9debc0fb708cbaeee7add1ab8434b99fe
mason.hows11 <mason.hows11@gmail.com>
2021-09-29 17:07:05 +0330

    add code to check linktime service

--- app/services/CheckLinkTime.php
+++ app/services/CheckLinkTime.php
@@ -4,10 +4,25 @@
 namespace App\services;
 
 
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\DB;
+
 class CheckLinkTime
 {
     public static function checkLinkExpireTime()
     {
+        $token = $request->route('token');
+        $email = $request->route('email');
+
+        $link = DB::table('password_resets')
+            ->where('token', '=', $token)
+            ->where('email', '=', $email)->select('created_at')->first();
+
+        $expired  = Carbon::parse($link->created_at)->addMinutes(60)->isPast();
+        if($expired)
+        {
+            return  redirect()->route('loginForm')->with('error','لینک منقضی شده است.');
+        }
 
     }
 }
