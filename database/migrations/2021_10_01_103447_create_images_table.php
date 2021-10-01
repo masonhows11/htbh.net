e08952f680fd4d8c055b64bb041528e02d7bb4c6
mason.hows11 <mason.hows11@gmail.com>
2021-10-01 11:05:44 +0330

    add one to one relationship image user

--- database/migrations/2021_10_01_103447_create_images_table.php
+++ database/migrations/2021_10_01_103447_create_images_table.php
@@ -18,7 +18,7 @@ class CreateImagesTable extends Migration
 
             $table->unsignedBigInteger('user_id')->nullable();
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
-            
+
             $table->string('image')->nullable();
             $table->timestamps();
         });
