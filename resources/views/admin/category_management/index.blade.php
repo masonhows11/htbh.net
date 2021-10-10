4ad0c8b779943d0a4c63ddec2b33a9e8b57af29c
mason.hows11 <mason.hows11@gmail.com>
2021-10-10 07:35:51 +0330

    ready for delete just delete not detach one category with ajax

--- resources/views/admin/category_management/index.blade.php
+++ resources/views/admin/category_management/index.blade.php
@@ -81,3 +81,60 @@
 
     </div>
 @endsection
+@section('admin_scripts')
+    <script>
+        $(document).on('click', '#deleteItem', function (event) {
+            event.preventDefault();
+            let role_id = event.target.getAttribute('data-role-id');
+            let role_element = event.target.closest('tr');
+            swal.fire({
+                title: 'آیا مطمئن هستید این ایتم حذف شود؟',
+                icon: 'error',
+                showCancelButton: true,
+                confirmButtonColor: '#3085d6',
+                cancelButtonColor: '#d33',
+                confirmButtonText: 'بله حذف کن!',
+                cancelButtonText: 'خیر',
+            }).then((result) => {
+                // confirmed scope start
+                if (result.isConfirmed) {
+                    // ajax scope start
+                    $.ajaxSetup({
+                        headers: {
+                            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                        }
+                    });
+                    $.ajax({
+                        method: 'GET',
+                        url: '{{ route('deleteRole') }}',
+                        data: {role_id: role_id},
+                    }).done(function (data) {
+                        if (data['status'] === 200) {
+                            role_element.remove();
+                            swal.fire({
+                                icon: 'success',
+                                text: data['success'],
+                            })
+                        }
+                        if (data['status'] === 404) {
+                            swal.fire({
+                                icon: 'warning',
+                                text: data['warning'],
+                            })
+                        }
+                    }).fail(function (data) {
+                        if (data['status'] === 500) {
+                            swal.fire({
+                                icon: 'error',
+                                text: data['error'],
+                            })
+                        }
+                    });
+                    // ajax scope end
+                }
+                // confirmed scope end
+            });
+        });
+    </script>
+@endsection
+
