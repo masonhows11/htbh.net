4a60ed85f354977d4c89d52e1592ce0ea3ff9306
mason.hows11 <mason.hows11@gmail.com>
2021-10-10 13:02:30 +0330

    category updated

--- resources/views/errors/error_not_found_model.blade.php
+++ resources/views/errors/error_not_found_model.blade.php
@@ -1,1 +1,13 @@
-<?php
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>خطای 404</title>
+</head>
+<body>
+<h1 style="text-align: center;color: #0b58a2">رکورد مورد نظر وجود ندارد...</h1>
+</body>
+</html>
--- resources/views/errors/error_store_model.blade.php
+++ resources/views/errors/error_store_model.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>خطای ذخیره سازس</title>
+    <title>خطای ذخیره سازی</title>
 </head>
 <body>
     <h1 style="text-align: center;color: #0b58a2">خطا در عملیات ذخیره سازی..</h1>
