e46e1fbb5ae9a972a69881e8ad02ed486453d3d9
mason.hows11 <mason.hows11@gmail.com>
2021-10-10 12:52:51 +0330

    category updated

--- app/Http/Controllers/Admin/AdminCategoryController.php
+++ app/Http/Controllers/Admin/AdminCategoryController.php
@@ -42,9 +42,9 @@ class AdminCategoryController extends Controller
                 ]);
                 return redirect()->back()->with('success', 'دسته بندی مورد با موفقیت ذخیره شد.');
             } catch (\Exception $ex) {
-                    return view('errors.store_error');
+                return view('errors.store_error');
             }
-        }else {
+        } else {
             try {
                 Category::create([
                     'name' => $request->name,
@@ -61,25 +61,59 @@ class AdminCategoryController extends Controller
 
     public function edit(Request $request)
     {
-
+        $category = Category::find($request->cat);
+        $categories = Category::all();
+        return view('admin.category_management.edit')->with(['category' => $category, 'categories' => $categories]);
     }
 
     public function update(Request $request)
     {
+        $request->validate([
+            'name' => 'required|min:3',
+            'title' => 'required|min:3',
+            'slug' => 'required|min:3',
+        ], $message = [
+            'name.required' => 'نام دسته بندی را وارد کنید',
+            'name.min' => ' حداکثر ۳ کاراکتر ',
 
+            'title.min' => ' حداکثر ۳ کاراکتر ',
+            'title.required' => 'عنوان دسته بندی را وارد کنید',
+
+            'slug.required' => 'اسلاگ به انگلیسی',
+            'slug.max' => ' حداکثر ۳ کاراکتر '
+        ]);
+
+
+        if ($request->has('parent')) {
+            Category::where('id', $request->id)
+                ->update(['name' => $request->name,
+                    'title' => $request->title,
+                    'slug' => $request->slug,
+                    'parent_id' => $request->parent]);
+
+            return redirect('/admin/category/index')->with('success', 'دسته بندی مورد با موفقیت ویرایش شد.');
+        } else
+            Category::where('id', $request->id)
+                ->update(['name' => $request->name,
+                    'title' => $request->title,
+                    'slug' => $request->slug]);
+        return redirect('/admin/category/index')->with('success', 'دسته بندی مورد با موفقیت ویرایش شد.');
     }
 
     public function detachParent(Request $request)
     {
-
+        $category = Category::findOrFail($request->cat);
+        $category->parent_id = null;
+        $category->save();
+        return redirect('/admin/category/index')->with('success', 'دسته بندی مورد نظر با موفقیت ویرایش شد.');
     }
+
     public function delete(Request $request)
     {
 
         try {
             $cat = Category::find($request->cat);
-        }catch (\Exception $ex)
-        {
+        } catch (\Exception $ex) {
             return view('errors.delete_error');
         }
         Category::destroy($request->cat);
