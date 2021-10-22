e0734b25f5bf70258523a4fae20c80e26365c1e4
mason.hows11 <mason.hows11@gmail.com>
2021-10-22 20:44:54 +0330

    ready for single page for article course

---
+++ app/Http/Controllers/Front/ArticleController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Front;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Post;
+class ArticleController extends Controller
+{
+    //
+}
