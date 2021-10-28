dc6d4328bb05a30c4fc083967218b2897b7ecf1c
mason <mason.hows11@gmail.com>
2021-10-28 13:01:45 +0330

    course comment admin updated

--- app/Http/Controllers/Admin/AdminCommentController.php
+++ app/Http/Controllers/Admin/AdminCommentController.php
@@ -3,20 +3,22 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Category;
 use Illuminate\Http\Request;
 
 class AdminCommentController extends Controller
 {
     //
-
+    public function getCoursesComments()
+    {
+        $categories = Category::all();
+        return view('admin.comment_management.courses')
+            ->with('categories',$categories);
+    }
     public function getPostsComments()
     {
         return view('admin.comment_management.posts');
     }
 
-    public function getCoursesComments()
-    {
-        return view('admin.comment_management.courses');
 
-    }
 }
--- resources/views/admin/comment_management/courses.blade.php
+++ resources/views/admin/comment_management/courses.blade.php
@@ -1,23 +1,35 @@
 @extends('admin.include.master')
 @section('page_title')
-مدیریت دیدگاه های دوره ها
+    مدیریت دیدگاه های دوره ها
 @endsection
 @section('main_content')
-<div class="container">
+    <div class="container">
+
+        <div class="row category-comments">
+            <div class="col-lg-10 select-category-comment" style="border:1px solid red;height:100px">
+
+                    <div class="form-group">
+                        <label for="se_cat">یک دسته بندی انتخاب کنید.</label>
+                        <select id="se_cat" class="select_category" name="select_category">
+                            @foreach($categories as $category)
+                                <option value="{{ $category->id }}">{{ $category->title }}</option>
+                            @endforeach
+                        </select>
+                    </div>
+               
+
+            </div>
+        </div>
+
 
         <div class="row">
-        
-                
-        </div>
-        
-
-        <div class="row">
 
 
         </div>
-</div>
+    </div>
 @endsection
 @section('admin_scripts')
+
 @endsection
 
 
