31f3d209358fa1200795f2f85cb8db25728232b9
mason.hows11 <mason.hows11@gmail.com>
2021-11-05 16:22:13 +0330

    season updated

--- resources/views/admin/season_management/create.blade.php
+++ resources/views/admin/season_management/create.blade.php
@@ -5,7 +5,19 @@
 @section('main_content')
 <div class="container">
 
-        
+    <div class="row alert-section">
+        <div class="col-lg-6 col-md-6 col-xs-6 alert-box">
+            @include('admin.include.alert')
+        </div>
+    </div>
+
+    <div class="row season_list">
+
+        <div class="col-lg-8 col-md-8 col-xs-8">
+
+        </div>
+
+    </div>
 
 </div>
 @endsection
