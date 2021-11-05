abff00dcc246df4c856c81ac2b4abc28e6e9796e
mason.hows11 <mason.hows11@gmail.com>
2021-11-05 16:35:24 +0330

    season updated

--- app/Models/Season.php
+++ app/Models/Season.php
@@ -4,18 +4,33 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Cviebrock\EloquentSluggable\Sluggable;
 
 class Season extends Model
 {
     protected $table = "seasons";
-    use HasFactory;
-    protected $fillable = [ 'title','name','course_id','lesson_Id'];
 
+    use HasFactory,Sluggable;
+
+    protected $fillable = [ 'title','name','course_id','lesson_id'];
+
+
+    public function sluggable(): array
+    {
+        return [
+            'slug' => [
+                'source' => 'name'
+            ]
+        ];
+    }
 
     public function course()
     {
         return $this->belongsTo(Course::class);
     }
+
+
+
 }
 
 
