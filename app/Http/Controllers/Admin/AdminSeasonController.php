bfd1d640921aaafd60db2e1148929f1ea7945ed9
mason.hows11 <mason.hows11@gmail.com>
2021-11-06 22:14:11 +0330

    disable debugbar

--- app/Http/Controllers/Admin/AdminSeasonController.php
+++ app/Http/Controllers/Admin/AdminSeasonController.php
@@ -14,17 +14,16 @@ class AdminSeasonController extends Controller
     public function create(Request $request)
     {
         $course = Course::with('seasons')
-            ->where('id','=',$request->course)->get();
+            ->where('id', '=', $request->course)->get();
 
 
         session()->forget('create_season');
         Session()->regenerate();
-        session()->put('create_season',$request->fullUrl());
-
+        session()->put('create_season', $request->fullUrl());
 
 
         return view('admin.season_management.create')
-            ->with(['course'=>$course]);
+            ->with(['course' => $course]);
     }
 
     public function store(Request $request)
@@ -34,23 +33,22 @@ class AdminSeasonController extends Controller
         $request->validate([
             'title' => 'required|max:20',
             'name' => 'required|max:20',
-        ],$messages=[
+        ], $messages = [
             'title.required' => 'عنوان را به فارسی وارد کنید.',
-            'title.max'=> 'حداکثر ۲۰ کاراکتر.',
+            'title.max' => 'حداکثر ۲۰ کاراکتر.',
             'name.required' => 'نام را به انگلیسی وارد کنید.',
-            'name.max'=> 'حداکثر ۲۰ کاراکتر.',
+            'name.max' => 'حداکثر ۲۰ کاراکتر.',
         ]);
         try {
             Season::create([
-                'title'=>$request->title,
-                'name'=>$request->name,
+                'title' => $request->title,
+                'name' => $request->name,
                 'course_id' => $request->course,
             ]);
             return redirect()
                 ->back()
-                ->with(['success'=>'فصل جدید با موفقیت ذخیره شد.']);
-        }catch (\Exception $ex)
-        {
+                ->with(['success' => 'فصل جدید با موفقیت ذخیره شد.']);
+        } catch (\Exception $ex) {
 
             return view('errors.error_store_model');
         }
@@ -64,13 +62,10 @@ class AdminSeasonController extends Controller
 
             $season = Season::findOrFail($request->season);
             return view('admin.season_management.edit')
-                ->with(['season'=>$season]);
-        }catch (\Exception $ex)
-        {
+                ->with(['season' => $season]);
+        } catch (\Exception $ex) {
             return view('errors.error_not_found_model');
         }
-
-
 
 
     }
@@ -81,38 +76,37 @@ class AdminSeasonController extends Controller
         $request->validate([
             'title' => 'required|max:20',
             'name' => 'required|max:20',
-        ],$messages=[
+        ], $messages = [
             'title.required' => 'عنوان را به فارسی وارد کنید.',
-            'title.max'=> 'حداکثر ۲۰ کاراکتر.',
+            'title.max' => 'حداکثر ۲۰ کاراکتر.',
             'name.required' => 'نام را به انگلیسی وارد کنید.',
-            'name.max'=> 'حداکثر ۲۰ کاراکتر.',
+            'name.max' => 'حداکثر ۲۰ کاراکتر.',
         ]);
 
         try {
-            Season::where('id','=',$request->season)->update([
-                'title'=>$request->title,
-                'name'=>$request->name,
+            Season::where('id', '=', $request->season)->update([
+                'title' => $request->title,
+                'name' => $request->name,
             ]);
 
-          if (session()->has('create_season')){
+            if (session()->has('create_season')) {
 
-              return redirect()->to(session()->get('create_season'));
+                return redirect()->to(session()->get('create_season'));
             }
-            return redirect()->back()->with(['success'=>'فصل با موفقیت ویرایش شد.']);
-        }catch (\Exception $ex)
-        {
+            return redirect()->back()->with(['success' => 'فصل با موفقیت ویرایش شد.']);
+        } catch (\Exception $ex) {
 
-           return view('errors.error_store_model');
+            return view('errors.error_store_model');
         }
 
     }
 
-    public function delete(Request  $request)
+    public function delete(Request $request)
     {
-        return $request;
+       
         $season = Season::where('id', '=', $request->season)
-                        ->where('course_id','=',$request->course)
-                        ->first();
+            ->where('course_id', '=', $request->course)
+            ->first();
         if (!$season) {
             return response()->json(['warning' => 'فصل مورد نظر وجود ندارد.', 'status' => 404], 200);
         }
