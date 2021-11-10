f48b5b0fa58410d21f033e38395bbaf4164eca3a
mason.hows11 <mason.hows11@gmail.com>
2021-11-10 19:58:06 +0330

    ready for front course coding

--- resources/views/front/course_page/course.blade.php
+++ resources/views/front/course_page/course.blade.php
@@ -171,7 +171,7 @@
                                 <div class="accordion-body">
                                     @foreach($item->lessons as $value)
                                         <ul class="list-group list-group-flush">
-                                            <li class="list-group-item"><a href="">{{ $value->title }}</a></li>
+                                            <li class="list-group-item"><a href="">{{ $value->title }}</a><i class="fa fa-lock-open float-end"></i></li>
                                         </ul>
                                     @endforeach
                                 </div>
