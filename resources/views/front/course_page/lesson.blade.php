44f50b140eee1e55a7e0d659dc100e6a7f7af0f3
mason.hows11 <mason.hows11@gmail.com>
2021-11-12 21:18:16 +0330

    lesson detail updated

--- resources/views/front/course_page/lesson.blade.php
+++ resources/views/front/course_page/lesson.blade.php
@@ -5,19 +5,22 @@
 @section('main_content')
     <div class="container ">
 
-        @foreach($lesson as $item)
+
         <div class="row d-flex flex-column align-content-center justify-content-center front-lesson-detail">
 
 
+
             <div class="col-lg-8 col-md-8 col-xs-8 mb-2 video-section" style="border:2px solid tomato;height:100px">
-                <p></p>
-                <p></p>
+                <h1 class="h4" class="lesson-title">{{ $lesson[0]->title }}</h1>
+                <p class="course-title">{{ $lesson[0]->course->title }}</p>
                 <div>
-                    <span></span>
-                    <span></span>
+                    <span class="lesson-date">{{  jdate($lesson[0]->created_at)->format('%d %B %Y')  }}</span>
+                    
+                    <span class="lesson-time">{{ $lesson[0]->lesson_duration }}</span>
                 </div>
 
             </div>
+
 
             <div class="col-lg-8 col-md-8 col-xs-8 video-section" style="border:2px solid tomato;height:600px">
 
@@ -32,7 +35,7 @@
         <div class="row col-lg-8 col-md-8 mt-5 mb-5 mx-auto col-xs-8" style="border:2px solid tomato;height: 200px">
 
         </div>
-        @endforeach
+
 
     </div>
 @endsection
