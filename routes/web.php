005be55bdf02a2f43b9118e54a1eca1c07516504
mason.hows11 <mason.hows11@gmail.com>
2021-11-12 15:44:07 +0330

    route lesson updated agin

--- routes/web.php
+++ routes/web.php
@@ -221,7 +221,7 @@ Route::group(['prefix'=>'course'],function (){
 
     Route::get('/get/{course}',[CourseController::class,'course'])->name('course');
 
-    Route::get('/lessonDetail/{course}/{lesson}',[CourseController::class,'lessonDetail'])->name('lessonDetail');
+    Route::get('/lesson_detail/{course}/{lesson}',[CourseController::class,'lessonDetail'])->name('lesson_detail');
 });
 
 Route::group(['prefix'=>'article'],function (){
