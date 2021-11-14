98b003e5aac3604407a278e2ff9c324a719e7fdb
masonhows11 <44483721+masonhows11@users.noreply.github.com>
2021-11-14 20:29:33 +0330

    applied some changes

--- app/Models/CourseUser.php
+++ app/Models/CourseUser.php
@@ -4,12 +4,22 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 
 class CourseUser extends Model
 {
     use HasFactory;
 
+    protected $table = 'course_user';
     protected $fillable = ['course_id','user_id'];
 
+    public static function checkAddOrNot($user,$course)
+    {
+        $added = DB::table('course_user')->where('user_id', '=', $user)->where('course_id','=',$course)->count();
+        if ($added > 0 ) {
+            return true;
+        }
+        return false;
+    }
 
 }
