890e47ca3738544752bcae7b1917138552505cd9
mason.hows11 <mason.hows11@gmail.com>
2021-11-16 15:09:45 +0330

    move controllers

--- app/Http/Controllers/Front/CourseUserController.php
+++ app/Http/Controllers/Front/CourseUserController.php
@@ -1,7 +1,8 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Front;
 
+use App\Http\Controllers\Controller;
 use App\Models\CourseUser;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
