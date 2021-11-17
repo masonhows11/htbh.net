7a8d5b2d21ce9849af9ca580822b642d11cc6c11
mason.hows11 <mason.hows11@gmail.com>
2021-11-17 18:04:27 +0330

    add course and their lessons for user

--- app/Http/Controllers/Front/CourseUserController.php
+++ app/Http/Controllers/Front/CourseUserController.php
@@ -6,19 +6,29 @@ use App\Http\Controllers\Controller;
 use App\Models\CourseUser;
 use App\Models\Lesson;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\Auth;
 
 class CourseUserController extends Controller
 {
     //
 
-    public function addCourseToUser(Request $request)
+    public function addCourseToUser(Request $request): \Illuminate\Http\RedirectResponse
     {
-       //return $request;
 
-       $lessons = Lesson::where('course_id',$request->course)->select('id')->get();
+        $lessons = Lesson::where('course_id', $request->course)->select('id')->get();
+        $course_id = $request->course;
 
-       return $lessons;
+
+        foreach ($lessons as $i => $iValue) {
+            $courseUser = new CourseUser();
+            $courseUser->user_id = Auth::id();
+            $courseUser->lesson_id = $lessons[$i]['id'];
+            $courseUser->course_id = $course_id;
+            $courseUser->save();
+        }
+
+        return redirect()->back();
     }
 
 
