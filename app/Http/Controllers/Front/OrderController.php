2f831638243f372590d0daf6293c9fa75ccce1da
mason.hows11 <mason.hows11@gmail.com>
2021-11-22 18:00:21 +0330

    pay updated

--- app/Http/Controllers/Front/OrderController.php
+++ app/Http/Controllers/Front/OrderController.php
@@ -14,29 +14,43 @@ class OrderController extends Controller
     //
 
 
-    public function addOrder(Request  $request)
+    public function addOrder(Request $request)
     {
-        $basket_count = Basket::where('user_id','=',Auth::id())->count();
+        $basket_count = Basket::where('user_id', '=', Auth::id())->count();
 
-        if ($basket_count == 0)
+        if ($basket_count == 0) {
+            return redirect()->with(['error' => 'سبد خرید شما خالی است.']);
+        }
+
+        $get_basket = Basket::where('user_id', '=', Auth::id())->get();
+
+
+        $order = new Order();
+        $order->user_id = $get_basket[0]->user_id;
+        $order->total_price = $request->total_price;
+        $order->is_paid = 0;
+        $order->save();
+
+
+        foreach ($get_basket as $item)
         {
-            return redirect()->with(['error'=>'سبد خرید شما خالی است.']);
+            $order_details = new OrderDetails();
+            $order_details->user_id = $item->user_id;
+            $order_details->course_id = $item->course_id;
+            $order_details->course_qty = $item->qty;
+            $order_details->course_price = $item->price;
+            $order_details->order_id = $order->id;
+            $order_details->save();
         }
 
-        $get_basket = Basket::where('user_id','=',Auth::id())->get();
-
-        foreach ($get_basket as $item){
-            $order = new Order();
-            $order->user_id = $item->user_id;
-            $order->total_price = $request->total_price;
-            $order->is_paid = 0;
-            $order->save();
-        }
+        
 
 
-        $order_details = new OrderDetails();
 
 
-        return $basket_count;
+
+
+
+        //return $basket_count;
     }
 }
