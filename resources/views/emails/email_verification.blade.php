9b503c52a532429b9c1105b8ca6618c8bc7ec0ac
mason.hows11 <mason.hows11@gmail.com>
2021-11-25 10:53:44 +0330

    Apparently the link was created correctly.

--- resources/views/emails/email_verification.blade.php
+++ resources/views/emails/email_verification.blade.php
@@ -3,10 +3,8 @@
     @component('mail::button',['url'=>route('verifyEmail',[$id,$code])])
         برای فعال سازی حساب کاربری خود کلیک کنید
     @endcomponent
-
     <p>لینک فعال سازی رو در آدرس بار مرورگر خود بچسبانید</p>
     <p>{{route('verifyEmail',[$id,$code])}}</p>
-
     {{ config('app.name') }}
 @endcomponent
 
