7ea237ba0bf7fe008c6f9863842759499f569534
mason.hows11 <mason.hows11@gmail.com>
2021-12-01 17:28:43 +0330

    changes applied

--- resources/views/admin/post_management/create.blade.php
+++ resources/views/admin/post_management/create.blade.php
@@ -5,8 +5,8 @@
 @section('main_content')
     <div class="container">
 
-        <div class="row alert-section">
-            <div class="col-lg-6 col-md-6 col-xs-6 alert-box">
+        <div class="row post-alert-section">
+            <div class="col-lg-6 col-md-6 col-xs-6 post-alert-box">
                 @include('admin.include.alert')
             </div>
         </div>
